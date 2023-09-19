3b6ee2fd652aa5fde587553af6ee6e5a0ff8b683
renatocouto1867 <renato.couto@estudante.ifto.edu.br>
2023-09-18 22:22:41 -0300

    inicio do servidor

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
-const http = require('http').createServer();
+
+//cria uma aplicação express
+const app = express();
+
+const httpServer = require('http').createServer(app);
+
+//diz a minha aplicação express para usar a pasta public como sendo pública
+app.use(express.static('public'));
 
 const PORT =8080
-http.listen(PORT,()=> console.log('Servidor iniciado na porta ' + PORT));
+httpServer.listen(PORT,()=> console.log('Servidor iniciado na porta ' + PORT));
+
+app.get('/',(req,res)=> res.sendFile(__dirname + '/index.html'));
