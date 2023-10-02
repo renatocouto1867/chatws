d9d7337db04828a792bb4df7857853738bdc5101
renatocouto1867 <renato.couto@estudante.ifto.edu.br>
2023-10-02 20:52:54 -0300

    final

--- public/index.js
+++ public/index.js
@@ -1,1 +1,32 @@
-console.log('teste servidor')
+window.addEventListener("load", () => {
+    const socket = io(); //
+    console.log('Conectado no servidor de websocket');
+
+
+    document.getElementById('form').addEventListener('submit', (evt) => {
+        const msg = document.getElementById('msg').value;
+
+        console.log("Enviado Mensagem " + msg); // só para teste
+
+        if (socket.login) {
+            socket.emit('chatmsg', msg)//cria um rotulo para a mensagem
+        }
+        else {
+            socket.emit('login', msg)
+            socket.login = msg
+
+            document.getElementById('msg').addEventListener('keydown', () => {
+                socket.emit('status', `${socket.login} está digitando...`)
+            })
+
+            document.getElementById('msg').addEventListener('keyup', () => {
+                socket.emit('status', ``)
+            })
+        }
+
+        evt.preventDefault();
+    })
+
+    socket.on('chatmsg', msg => document.getElementById('mensagens').innerHTML += `<li>${msg}</li>`)
+    socket.on('status', msg=> document.getElementById('status').innerHTML=msg)
+})
--- server.js
+++ server.js
@@ -19,11 +19,20 @@ app.get('/ola',(req,res)=> res.send("Ola Mundo"));
 const PORT =8080
 
 serverSocket.on('connect', socket=>{
+
     console.log('Cliente conectado' + socket.id);
+    
+    socket.on('status', msg=> socket.broadcast.emit('status', msg))
+
+    socket.on('login', msg =>{
+        socket.login=msg;
+        serverSocket.emit('chatmsg',`${socket.login} Conectou`);
+    })
+
     //ao chegar uma mensagem tipo chatmsg do cliente faça
     socket.on('chatmsg', (msg) =>{
         console.log(`Mesagem, recebida do cliente ${socket.id}`);
-        serverSocket.emit('cahtmsg',msg);
+        serverSocket.emit('chatmsg',`${socket.login}: ${msg}`);
     })
 })
 
